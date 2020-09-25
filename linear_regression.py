463f6cb23947820865f449a5ce3424f961c8615a
RogueSTC <54365425+RogueSTC@users.noreply.github.com>
2020-09-25 08:28:19 -0500

    initial commit

---
+++ linear_regression.py
@@ -0,0 +1,81 @@
+#Michael Rao
+#1001558150
+
+import os
+import sys
+import numpy 
+
+#Load file 
+def load_file(file_name):
+    dataset = list()
+    i = 0
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            i = i + 1
+            dataset.append(line.split())
+    return dataset, i
+
+#Convert string to float
+def create_floats(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+# Calculate activation function matrix
+def act_funct(dataset, degree):
+    act_matr = list()
+
+    for x in range(len(dataset)):
+        act_vect = list()
+        for y in range(degree + 1):
+            act_vect.append(dataset[x][0]**(y))
+        act_matr.append(act_vect)
+    return(act_matr)
+
+def linear_regression(training_file, test_file, degree, lambda_in):
+    training_set, train_size = load_file(training_file)
+    test_set, test_size = load_file(test_file)
+    test_label = list()
+    t = list()
+
+    for i in range(len(training_set[0])):
+        create_floats(training_set, i)
+
+    for i in range(len(test_set[0])):
+        create_floats(test_set, i)
+
+    for i in range(len(test_set)):
+        test_label.append(test_set[i][-1])
+        del(test_set[i][-1])
+
+    for i in range(len(training_set)):
+        t.append(training_set[i][-1])
+    
+    test_set = numpy.array(act_funct(test_set,degree))
+    t = numpy.array(t)
+    act_matrix = numpy.array(act_funct(training_set, degree))
+
+    # Identity matrix
+    Id_mat = numpy.identity(act_matrix.shape[1])
+    
+    #phi transpose times phi
+    phiT_x_phi = numpy.matmul(numpy.transpose(act_matrix),act_matrix)
+
+    #lambda times ident matrix
+    lambda_ID = lambda_in * Id_mat
+
+    #phi transpose times t
+    phiT_t = numpy.matmul(numpy.transpose(act_matrix),t)
+    w = numpy.matmul(numpy.linalg.pinv(numpy.add(lambda_ID,phiT_x_phi)),phiT_t)
+
+    for i in range(len(w)):
+        print("w{0:d}={1:.4f}".format(i,w[i]))
+    print(test_set)
+    print(test_label)
+
+
+    for i in range(len(test_set)):
+        output = numpy.matmul(test_set[i], w)
+        print("ID={0:5d}, output={1:14.4f}, target value =  {2:14.4f}, squared error = {3:14.4f}".format(i+1, output,test_label[i], (test_label[i] - output)**2))
+   
+linear_regression("C:/Users/micha/Desktop/Fall 2020/Machine Learning/Assignment 3/f1.txt","C:/Users/micha/Desktop/Fall 2020/Machine Learning/Assignment 3/f1.txt",1,0)
