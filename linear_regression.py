943a6c81b9836e3a0d3d347318e110edebda637d
MikeDRao <michael.rao@mavs.uta.edu>
2020-09-30 16:50:00 -0500

    implemented command line args

--- linear_regression.py
+++ linear_regression.py
@@ -32,7 +32,16 @@ def act_funct(dataset, degree):
         act_matr.append(act_vect)
     return(act_matr)
 
-def linear_regression(training_file, test_file, degree, lambda_in):
+def linear_regression():
+    if(len(sys.argv) < 5):
+        print("Insufficient command line args")
+        exit()
+
+    training_file = sys.argv[1]
+    test_file = sys.argv[2]
+    degree = int(sys.argv[3])
+    lambda_in = int(sys.argv[4])
+
     training_set, train_size = load_file(training_file)
     test_set, test_size = load_file(test_file)
     test_label = list()
@@ -70,12 +79,10 @@ def linear_regression(training_file, test_file, degree, lambda_in):
 
     for i in range(len(w)):
         print("w{0:d}={1:.4f}".format(i,w[i]))
-    print(test_set)
-    print(test_label)
-
 
     for i in range(len(test_set)):
         output = numpy.matmul(test_set[i], w)
         print("ID={0:5d}, output={1:14.4f}, target value =  {2:14.4f}, squared error = {3:14.4f}".format(i+1, output,test_label[i], (test_label[i] - output)**2))
    
-linear_regression("C:/Users/micha/Desktop/Fall 2020/Machine Learning/Assignment 3/f1.txt","C:/Users/micha/Desktop/Fall 2020/Machine Learning/Assignment 3/f1.txt",1,0)
+if __name__ == '__main__':
+    linear_regression()
